d2a6397910cf489796b1f422f28440c3698a2097
El hamd <El hamd@DESKTOP-ANPFBI8>
2025-03-16 19:56:22 +0200

    first push of DAL

---
+++ Company.Ps1.DAL/Data/Config/DepartmentConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Ps1.DAL.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Company.Ps1.DAL.Data.Config
+{
+    internal class DepartmentConfig : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(d => d.Id).UseIdentityColumn(10, 10);
+        }
+    }
+}
