d6facb526a37af927b121dcf940470270971af9e
El hamd <El hamd@DESKTOP-ANPFBI8>
2025-03-19 18:53:47 +0200

    after finish all including task details

--- Company.Ps1.PL/Controllers/DepartmentController.cs
+++ Company.Ps1.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.Ps1.BIL.Repositeries;
 using Company.Ps1.BIL.Repositry;
+using Company.Ps1.DAL.Model;
+using Company.Ps1.PL.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Ps1.PL.Controllers
@@ -15,6 +17,37 @@ namespace Company.Ps1.PL.Controllers
         public IActionResult Index()
         {
           var result =  _repo.getall();
+            return View(result);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] //from client side to server
+        public IActionResult Create(DepartmentDTO departmentDTO)
+        {
+            if (ModelState.IsValid)
+            { 
+                var department = new Department() 
+                { 
+                    Code = departmentDTO.Code,
+                    Name = departmentDTO.Name,
+                    dateTime = departmentDTO.dateTime,
+                };
+               var x =  _repo.add(department);
+                if (x == 1)
+                { 
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(departmentDTO);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var result = _repo.Get(id);
             return View(result);
         }
     }
---
+++ Company.Ps1.PL/DTO/DepartmentDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Ps1.PL.DTO
+{
+    public class DepartmentDTO
+    {
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Code is required.")]
+        public string Code { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "datetime is required.")]
+        public DateTime dateTime { get; set; }
+
+    }
+}
